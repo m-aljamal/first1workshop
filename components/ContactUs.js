59074f2e71939672b9f78a5661ac00a89e1d93fc
m-aljamal <mohammadjamol@gmail.com>
2022-05-07 15:03:33 +0300

    form

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -1,37 +1,53 @@
 import React from "react";
-
+import Image from "next/image";
 const ContactUs = () => {
   return (
-    <div className="bg-gray-300 py-20 ">
+    <div className="py-20 container ">
       <h3 className="text-red-700 text-center text-xl tracking-wider">
         Get In Touch
       </h3>
       <h2 className="text-gray-700 font-bold text-4xl text-center py-4">
         Contact Us
       </h2>
-      <div>
-        <form>
-          <div className="flex">
-            <div>
-              <label className="text-red-700 text-lg font-bold">Name</label>
-              <input type="text" className="border border-red-700 p-2" />
+      <div className="flex">
+        <form className="w-1/2 bg-gray-200 p-4 rounded-lg mt-8">
+          <div className="flex gap-5 ">
+            <div className="w-full">
+              <label className="text-red-700 text-lg ">Name</label>
+              <input
+                type="text"
+                className="border border-red-700 p-2 w-full"
+                placeholder="Name"
+              />
             </div>
-            <div>
-              <label className="text-red-700 text-lg font-bold">Email</label>
-              <input type="text" className="border border-red-700 p-2" />
+            <div className="w-full">
+              <label className="text-red-700 text-lg ">Email</label>
+              <input
+                type="text"
+                className="border border-red-700 p-2 w-full"
+                placeholder="Email"
+              />
             </div>
           </div>
-          <div>
-            <label className="text-red-700 text-lg font-bold">Message</label>
-            <textarea className="border border-red-700 p-2" />
+          <div className="mt-2">
+            <label className="text-red-700 text-lg ">Message</label>
+            <textarea
+              className="border border-red-700 p-2 w-full"
+              placeholder="Message"
+            />
           </div>
           <div className="flex justify-center">
-            <button className="bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+            <button
+              className="bg-red-700 text-white  py-2 px-4 rounded-full mt-2 "
+              type="submit"
+            >
               Submit
             </button>
-          </div>+
+          </div>
         </form>
-        <div></div>
+        <div className=" relative   w-1/4  flex-1">
+          <Image src="/email.png" layout="fill" objectFit="contain" />
+        </div>
       </div>
     </div>
   );
