28fc73021fdd01a3e19ab0cc6d114a3b51e1f879
m-aljamal <mohammadjamol@gmail.com>
2022-05-09 22:33:11 +0300

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
